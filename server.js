ff58e19469cd902e13a35a6c3b3da0bae32af1f8
Pedro <pedrogoku80@gmail.com>
2024-10-03 01:14:33 -0300

    API node com postgres

---
+++ server.js
@@ -0,0 +1,50 @@
+import { fastify } from 'fastify'
+import { DataBasePostgres } from './database-postgres.js'
+import 'dotenv/config'
+
+const server = fastify()
+
+const database = new DataBasePostgres()
+
+server.post('/videos', async (request, refly) => {
+    const {title, description, duration} = request.body
+    await database.create({
+        title: title,
+        description: description,
+        duration: duration
+    })
+
+    return refly.status(201).send()
+})
+
+server.get('/videos', async (request, reply) => {
+    const search = request.query.search
+    const videos = await database.list(search)
+
+    return reply.send(videos)
+})
+
+server.put('/videos/:id', async (request, reply) => {
+    const videoId = request.params.id
+
+    const {title, description, duration} = request.body
+     await database.update(videoId, {
+        title: title,
+        description: description,
+        duration: duration
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id',  async (request, reply) => {
+    const videoId = request.params.id
+    await database.delete(videoId)
+
+    return reply.status(204).send()
+})
+
+
+server.listen({
+    port: process.env.PORT ?? 3333
+})
